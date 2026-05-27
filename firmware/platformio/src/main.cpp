3f3408c4d1c9239ca94eff290e4a570445a297f3
JoaoPedroCardosodosSantos <joaopedrocardosodossantos223@gmail.com>
2026-05-27 17:22:06 -0300

    feat: integrar fluxo completo teclado T9 editor

--- firmware/platformio/src/main.cpp
+++ firmware/platformio/src/main.cpp
@@ -1,10 +1,66 @@
 #include <Arduino.h>
 
+#include "teclado/teclado.h"
+
+#include "teclado/t9.h"
+
+#include "teclado/editor.h"
+
+Teclado teclado;
+
+T9 t9;
+
+Editor editor;
+
 void setup()
 {
     Serial.begin(9600);
+
+    teclado.iniciar();
 }
 
 void loop()
 {
+    char tecla=
+        teclado.ler();
+
+    if(!tecla)
+        return;
+
+    if(
+        tecla>='2' &&
+        tecla<='9'
+    )
+    {
+        bool repetir=
+            t9.mesmaSequencia(
+                tecla
+            );
+
+        char letra=
+            t9.converter(
+                tecla
+            );
+
+        if(repetir)
+        {
+            editor.substituirUltimo(
+                letra
+            );
+        }
+        else
+        {
+            editor.inserir(
+                letra
+            );
+        }
+
+        Serial.print(
+            "MSG: "
+        );
+
+        Serial.println(
+            editor.obter()
+        );
+    }
 }
